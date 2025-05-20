7078df4b20b25d705c1ea72994490d5f8e097b1f
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2025-05-20 14:48:03 -0700

    extended to handle all mysql field types

--- resources/leiningen/new/ls/crud.clj
+++ resources/leiningen/new/ls/crud.clj
@@ -35,10 +35,9 @@
          :elideSetAutoCommits             true
          :alwaysSendSetIsolation          false
          :enableQueryTimeouts             false
-         :zeroDateTimeBehavior            "CONVERT_TO_NULL"}) ; Database connection
+         :zeroDateTimeBehavior            "CONVERT_TO_NULL"})
 
-(def SALT "897sdn9j98u98kj")                                ; encryption salt for DB                            ; encryption salt for DB
-
+(def SALT "897sdn9j98u98kj")
 (def KEY (byte-array 16))
 
 (defn Query
@@ -83,7 +82,9 @@
 
 (defn crud-fix-id
   [v]
-  (if (clojure.string/blank? v) (Integer. 0) v))
+  (try
+    (if (clojure.string/blank? v) 0 (Long/parseLong (str v)))
+    (catch Exception _ 0)))
 
 (defn crud-capitalize-words
   "Capitalize words"
@@ -93,16 +94,15 @@
        (clojure.string/join)))
 
 (defn crud-format-date-internal
-  "Convert a MM/dd/yyyy format date to yyyy-MM-dd format using a string as a date
-   eg. 02/01/1997 -> 1997-02-01"
   [s]
-  (if (not-empty s)
-    (.format
-     (SimpleDateFormat. "yyyy-MM-dd")
-     (.parse
-      (SimpleDateFormat. "MM/dd/yyyy") s))
-
-    nil))
+  (try
+    (if (not-empty s)
+      (.format
+       (SimpleDateFormat. "yyyy-MM-dd")
+       (.parse
+        (SimpleDateFormat. "MM/dd/yyyy") s))
+      nil)
+    (catch Exception _ nil)))
 
 (defn get-table-describe
   [table]
@@ -112,29 +112,96 @@
   [table]
   (map #(keyword (:field %)) (get-table-describe table)))
 
-(defn get-table-types [table]
+(defn get-table-types
+  [table]
   (map #(keyword (:type %)) (get-table-describe table)))
 
 (defn process-field
   [params field field-type]
   (let [value (str ((keyword field) params))
-        field-type (st/lower-case field-type)]
+        field-type (-> field-type st/lower-case (st/replace #"\(.*\)" ""))]
     (cond
-      (st/includes? field-type "varchar") value
+      ;; String types
+      (or (st/includes? field-type "varchar")
+          (st/includes? field-type "text")
+          (st/includes? field-type "enum")
+          (st/includes? field-type "set")) value
+
       (st/includes? field-type "char") (st/upper-case value)
-      (st/includes? field-type "int") (if (clojure.string/blank? value) 0 value)
-      ;;(st/includes? field-type "date") (crud-format-date-internal value)
-      :else value)))
+
+      ;; Integer types
+      (or (st/includes? field-type "int")
+          (st/includes? field-type "tinyint")
+          (st/includes? field-type "smallint")
+          (st/includes? field-type "mediumint")
+          (st/includes? field-type "bigint"))
+      (cond
+        (clojure.string/blank? value) 0
+        (re-matches #"(?i)true" value) 1
+        (re-matches #"(?i)on" value) 1
+        (re-matches #"(?i)false" value) 0
+        (re-matches #"(?i)off" value) 0
+        (re-matches #"^-?\d+$" value) (try (Long/parseLong value) (catch Exception _ 0))
+        :else 0)
+
+      ;; Floating point types
+      (or (st/includes? field-type "float")
+          (st/includes? field-type "double")
+          (st/includes? field-type "decimal"))
+      (cond
+        (clojure.string/blank? value) 0.0
+        (re-matches #"^-?\d+(\.\d+)?$" value) (try (Double/parseDouble value) (catch Exception _ 0.0))
+        :else 0.0)
+
+      ;; Date/time types
+      (st/includes? field-type "year")
+      (if (clojure.string/blank? value)
+        nil
+        (subs value 0 4))
+
+      (or (st/includes? field-type "date")
+          (st/includes? field-type "datetime")
+          (st/includes? field-type "timestamp"))
+      (if (clojure.string/blank? value) nil value)
+
+      (st/includes? field-type "time")
+      (if (clojure.string/blank? value) nil value)
+
+      ;; Binary/blob/json types
+      (or (st/includes? field-type "blob")
+          (st/includes? field-type "binary")
+          (st/includes? field-type "varbinary")) value
+
+      (st/includes? field-type "json")
+      (if (clojure.string/blank? value) nil value)
+
+      ;; Boolean/bit types
+      (or (st/includes? field-type "bit")
+          (st/includes? field-type "bool")
+          (st/includes? field-type "boolean"))
+      (cond
+        (clojure.string/blank? value) false
+        (re-matches #"(?i)true" value) true
+        (re-matches #"(?i)on" value) true
+        (re-matches #"(?i)false" value) false
+        (re-matches #"(?i)off" value) false
+        (re-matches #"^-?\d+$" value) (not= value "0")
+        :else false)
+
+      ;; Default fallback
+      :else
+      ;; (println "Warning: Unhandled MySQL type" field-type "for field" field)
+      value)))
 
 (defn build-postvars
   "Build post vars for table and process by type"
   [table params]
   (let [td (get-table-describe table)]
     (into {}
-          (map (fn [x]
-                 (when ((keyword (:field x)) params)
-                   {(keyword (:field x))
-                    (process-field params (:field x) (:type x))})) td))))
+          (keep (fn [x]
+                  (when ((keyword (:field x)) params)
+                    {(keyword (:field x))
+                     (process-field params (:field x) (:type x))})) td))))
 
 (defn build-form-field
   [d]
@@ -147,52 +214,70 @@
 
 (defn get-table-key
   [d]
-  (:field (first (filter #(= (:key %) "PRI") d))))
+  (when (seq d)
+    (when-let [pk (first (filter #(= (:key %) "PRI") d))]
+      (:field pk))))
 
 (defn build-form-row
   "Builds form row"
   [table id]
-  (let [tid (get-table-key (get-table-describe table))
-        head "SELECT "
-        body (apply str (interpose #"," (map #(build-form-field %) (get-table-describe table))))
-        foot (str " FROM " table " WHERE " tid " = ?")
-        sql (str head body foot)
-        row (Query db [sql id])]
-    (first row)))
+  (let [describe (get-table-describe table)
+        tid (get-table-key describe)]
+    (when tid
+      (let [head "SELECT "
+            body (apply str (interpose #"," (map #(build-form-field %) describe)))
+            foot (str " FROM " table " WHERE " tid " = ?")
+            sql (str head body foot)
+            row (Query db [sql id])]
+        (first row)))))
+
+(defn blank->nil
+  [m]
+  (into {}
+        (for [[k v] m]
+          [k (if (and (string? v) (clojure.string/blank? v)) nil v)])))
 
 (defn remove-emptys
   [postvars]
-  (apply dissoc postvars (for [[k v] postvars :when (empty? v)] k)))
+  (if (map? postvars)
+    (apply dissoc postvars
+           (for [[k v] postvars :when (nil? v)] k))
+    {}))
 
 (defn process-regular-form
   "Standard form save ex. (build-for-save params 'eventos')"
   [params table]
   (let [id (crud-fix-id (:id params))
-        postvars (build-postvars table params)
-        postvars (if (= id 0) (dissoc postvars :id) postvars)
-        postvars (remove-emptys postvars)
-        result (Save db (keyword table) postvars ["id = ?" id])]
-    (if (seq result) true false)))
+        postvars (cond-> (-> (build-postvars table params)
+                             blank->nil)
+                   (= id 0) (dissoc :id))]
+    (if (and (map? postvars) (seq postvars))
+      (let [result (Save db (keyword table) postvars ["id = ?" id])]
+        (if (seq result) true false))
+      false)))
 
-;; Start upload form
 (defn crud-upload-image
   "Uploads image and renames it to the id passed"
   [table file id path]
-  (let [tempfile   (:tempfile file)
-        size       (:size file)
-        type       (:content-type file)
-        extension  (peek (clojure.string/split type #"\/"))
-        extension  (if (= extension "jpeg") "jpg" "jpg")
-        image-name (str table "_" id "." extension)]
-    (when-not (zero? size)
-      (io/copy tempfile (io/file (str path image-name))))
-    image-name))
+  (if (and file (:tempfile file) (:size file) (:content-type file))
+    (let [tempfile   (:tempfile file)
+          size       (:size file)
+          type       (:content-type file)
+          extension  (last (clojure.string/split type #"/"))
+          extension  (if (= extension "jpeg") "jpg" extension)
+          image-name (str table "_" id "." extension)]
+      (when (and tempfile (not (zero? size)))
+        (io/copy tempfile (io/file (str path image-name))))
+      image-name)
+    nil))
 
-(defn get-id [id postvars table]
+(defn get-id
+  [id postvars table]
   (if (= id 0)
-    (-> (Save db (keyword table) postvars ["id = ?" id])
-        first
-        :generated_key)
+    (when (map? postvars)
+      (-> (Save db (keyword table) postvars ["id = ?" id])
+          first
+          :generated_key))
     id))
 
 (defn process-upload-form
@@ -201,14 +286,15 @@
         file (:file params)
         postvars (dissoc (build-postvars table params) :file)
         postvars (if (= id 0) (dissoc postvars :id) postvars)
-        postvars (remove-emptys postvars)
-        the-id (str (get-id id postvars table))
-        path (str (:uploads config) folder "/")
-        image-name (crud-upload-image table file the-id path)
-        postvars (assoc postvars :imagen image-name :id the-id)
-        result (Save db (keyword table) postvars ["id = ?" the-id])]
-    (if (seq result) true false)))
-;; End upload form
+        postvars (-> postvars blank->nil)]
+    (if (and (map? postvars) (seq postvars))
+      (let [the-id (str (get-id id postvars table))
+            path (str (:uploads config) folder "/")
+            image-name (crud-upload-image table file the-id path)
+            postvars (assoc postvars :imagen image-name :id the-id)
+            result (Save db (keyword table) postvars ["id = ?" the-id])]
+        (if (seq result) true false))
+      false)))
 
 (defn build-form-save
   "Builds form save"
@@ -219,10 +305,11 @@
 
 (defn build-form-delete
   [table id]
-  (let [result (if-not (nil? id)
+  (let [result (if (and (not (nil? id)) (not (clojure.string/blank? (str id))))
                  (Delete db (keyword table) ["id = ?" id])
                  nil)]
     (if (seq result) true false)))
+
 
 (comment
   (Query db "select * from users"))
